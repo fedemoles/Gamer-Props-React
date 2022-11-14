c27882cb6ce3fa314e084c5e2cf27f823e6feae0
fedemoles <fededevcod@gmail.com>
2022-11-14 11:35:49 -0300

    corrección SSD

--- src/components/Navlinks/NavLinks.jsx
+++ src/components/Navlinks/NavLinks.jsx
@@ -10,7 +10,7 @@ const NavLinks = () => {
                 <Nav.Link className="linkNav animate__animated animate__fadeIn animate__delay-1s" href="/category/motherboard">MOTHERBOARD</Nav.Link>
                 <Nav.Link className="linkNav animate__animated animate__fadeIn animate__delay-1s" href="/category/processor">PROCESSOR</Nav.Link>
                 <Nav.Link className="linkNav animate__animated animate__fadeIn animate__delay-1s" href="/category/memory">MEMORY</Nav.Link>
-                <Nav.Link className="linkNav animate__animated animate__fadeIn animate__delay-1s" href="/category/storage">SDD/M2/HDD</Nav.Link>
+                <Nav.Link className="linkNav animate__animated animate__fadeIn animate__delay-1s" href="/category/storage">SSD/M2/HDD</Nav.Link>
                 <Nav.Link className="linkNav animate__animated animate__fadeIn animate__delay-1s" href="/category/gpu">GPU</Nav.Link>
             </Nav>
         </div>
