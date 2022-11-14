1f5e58d98bd3e309e70d13dbd052054df9dffd51
fedemoles <fededevcod@gmail.com>
2022-11-14 14:28:12 -0300

    corrección NavLinks

--- src/components/Navlinks/NavLinks.jsx
+++ src/components/Navlinks/NavLinks.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
-import Nav from 'react-bootstrap/Nav';
-import './NavLinks.css'
-//import { Link } from "react-router-dom"; <Link to="/category/smartphones">Smartphones</Link>
+import React from "react";
+import Nav from "react-bootstrap/Nav";
+import "./NavLinks.css";
+import { Link } from "react-router-dom";
 
 const NavLinks = () => {
-    return (
-        <div className="containerLinks">
-            <Nav className="menuNav animate__animated animate__fadeIn animate__delay-1s">
-                <Nav.Link className="linkNav animate__animated animate__fadeIn animate__delay-1s" href="/category/motherboard">MOTHERBOARD</Nav.Link>
-                <Nav.Link className="linkNav animate__animated animate__fadeIn animate__delay-1s" href="/category/processor">PROCESSOR</Nav.Link>
-                <Nav.Link className="linkNav animate__animated animate__fadeIn animate__delay-1s" href="/category/memory">MEMORY</Nav.Link>
-                <Nav.Link className="linkNav animate__animated animate__fadeIn animate__delay-1s" href="/category/storage">SSD/M2/HDD</Nav.Link>
-                <Nav.Link className="linkNav animate__animated animate__fadeIn animate__delay-1s" href="/category/gpu">GPU</Nav.Link>
-            </Nav>
-        </div>
-    );
-}
+  return (
+    <div className="containerLinks">
+      <Nav className="menuNav animate__animated animate__fadeIn animate__delay-1s">
+        <Nav.Link as={Link} to="/category/motherboard">
+          MOTHERBOARD
+        </Nav.Link>
+        <Nav.Link as={Link} to="/category/processor">
+          PROCESSOR
+        </Nav.Link>
+        <Nav.Link as={Link} to="/category/memory">
+          MEMORY
+        </Nav.Link>
+        <Nav.Link as={Link} to="/category/storage">
+          SSD/M2/HDD
+        </Nav.Link>
+        <Nav.Link as={Link} to="/category/gpu">
+          GPU
+        </Nav.Link>
+      </Nav>
+    </div>
+  );
+};
 
 export default NavLinks;
